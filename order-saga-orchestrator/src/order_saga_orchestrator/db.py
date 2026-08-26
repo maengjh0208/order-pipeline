6c899fe68eb0c760a87a08dbe93a2e03a9635607
maengjh <juhee.bda@gmail.com>
2026-08-26 23:22:58 +0900

    docs: CLAUDE.md 수정

--- order-saga-orchestrator/src/order_saga_orchestrator/db.py
+++ order-saga-orchestrator/src/order_saga_orchestrator/db.py
@@ -12,7 +12,7 @@ engine = create_async_engine(settings.DATABASE_URL)
 async_session_factory = async_sessionmaker(
     engine,
     # expire_on_commit : SQLAlchemy 세션은 기본적으로 expire_on_commit=True로, commit()을 호출하면 세션이 추적하던 모든 객체를 expire(만료) 상태로 만든다.
-    # 만료된 객체의 속성에 접근하면 SQLAlchemy는 최신 데이터를 다시 가져오기 위해 자동으로 DB에 SELECT쿼리를 다시 날린다.
+    # 만료된 객체의 속성에 접근하면 SQLAlchemy는 최신 데이터를 다시 가져오기 위해 자동으로 DB에 SELECT 쿼리를 다시 날린다.
     expire_on_commit=False,
 )
 
